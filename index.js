b9ebf50706ecd3279cffc2f17dd30063da4cccef
Arjun <dotadexarjun2002@gmail.com>
2023-01-08 11:59:28 +0530

    added js files

---
+++ index.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".auto-type", {
+    strings: ["One glance at a book and you hear the voice of another person, perhaps someone dead for 1,000 years. To read is to voyage through time. – Carl Sagan", "A book is a garden, an orchard, a storehouse, a party, a company by the way, a counselor, a multitude of counselors. – Charles Baudelaire", "Literature is the safe and traditional vehicle through which we learn about the world and pass on values from one generation to the next. Books save lives. – Laurie Anderson", "That is part of the beauty of all literature. You discover that your longings are universal longings, that you’re not lonely and isolated from anyone. You belong. – F. Scott Fitzgerald"],
+    typeSpeed: 40,
+    backspeed: 40,
+    loop: true
+})
